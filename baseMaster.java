2f4137c0f75566d961cf52908f429fe19d3c5431
Ricky Harris <rickyh8198@msn.com>
2014-09-16 14:16:14 -0400

    initial

--- baseMaster.java
+++ baseMaster.java
@@ -1,33 +1,12 @@
 
-/**
- * Write a description of class baseMaster here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class baseMaster
+
+public class BaseMaster
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class baseMaster
-     */
-    public baseMaster()
+    
+    public BaseMaster()
     {
-        // initialise instance variables
-        x = 0;
+        
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
