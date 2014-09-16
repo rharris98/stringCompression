6f905dc9bf48f74b4fef7562505bc42e07037719
Ricky Harris <rickyh8198@msn.com>
2014-09-16 14:03:42 -0400

    initial commit

---
+++ baseMaster.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class baseMaster here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class baseMaster
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class baseMaster
+     */
+    public baseMaster()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ reviewingBasics.java
@@ -0,0 +1,14 @@
+
+public class reviewingBasics
+{
+  
+    public reviewingBasics()
+    {
+      String f = "f";
+      String f2 = new String( "f");
+      System.out.println(f==f2);
+      
+    }
+
+ 
+}
